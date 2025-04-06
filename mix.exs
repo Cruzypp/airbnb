fc5dd8e58a8ba8557ed77f3732cc41bfbabc3c56
Cruz P <A01738823@tec.mx>
2025-04-06 12:33:35 -0600

    first commit

---
+++ mix.exs
@@ -0,0 +1,25 @@
+defmodule Airbnb.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :airbnb,
+      version: "0.1.0",
+      elixir: "~> 1.18",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    [
+      {:csv, "~> 3.2"}
+    ]
+  end
+end
